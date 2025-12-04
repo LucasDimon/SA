827598ce7f8fab2b6300311e98ecfea6967ba8be
LucasDimon <lucas_dimon@estudante.sesisenai.org.br>
2025-12-04 00:25:26 -0300

    Instalando pg e versell completo

--- backend/src/routes/animals.routes.js
+++ backend/src/routes/animals.routes.js
@@ -1,14 +1,23 @@
-import { Router } from 'express';
-import multer from 'multer';
-import { listAnimals, getAnimal, createAnimal, updateAnimal, deleteAnimal } from '../controllers/animalController.js';
-import { authRequired, adminRequired } from '../middleware/auth.js';
+import { Router } from "express";
+import multer from "multer";
+import {
+  listAnimals,
+  getAnimal,
+  createAnimal,
+  updateAnimal,
+  deleteAnimal,
+} from "../controllers/animalController.js";
+import { authRequired, adminRequired } from "../middleware/auth.js";
 
-const upload = multer({ dest: 'src/uploads/' });
+const upload = multer({ dest: "/tmp/uploads/" }); // << TROQUEI AQUI
 
 const router = Router();
-router.get('/', listAnimals);
-router.get('/:id', getAnimal);
-router.post('/', authRequired, upload.single('foto'), createAnimal);
-router.put('/:id', authRequired, upload.single('foto'), updateAnimal);
-router.delete('/:id', authRequired, adminRequired, deleteAnimal);
+
+router.get("/", listAnimals);
+router.get("/:id", getAnimal);
+router.post("/", authRequired, upload.single("foto"), createAnimal);
+router.put("/:id", authRequired, upload.single("foto"), updateAnimal);
+router.delete("/:id", authRequired, adminRequired, deleteAnimal);
+
 export default router;
+
