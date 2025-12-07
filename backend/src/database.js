901ca4a42a91754b3dadf556961cb0a89b87ae24
LucasDimon <lucas_dimon@estudante.sesisenai.org.br>
2025-12-07 15:51:32 -0300

    Reorganizando monorepo PetAdote (backend + frontend)

--- backend/src/database.js
+++ backend/src/database.js
@@ -4,16 +4,13 @@ import pg from 'pg'; // Importação obrigatória para o Vercel
 
 const DATABASE_URL = process.env.DATABASE_URL;
 
-// Verifica se a URL existe
 if (!DATABASE_URL) {
   throw new Error('DATABASE_URL not defined no .env');
 }
 
-// AQUI ESTÁ A CORREÇÃO: "export const conexao"
-// O erro acontecia porque essa palavra "export" ou "const" podia estar faltando
 export const conexao = new Sequelize(DATABASE_URL, {
   dialect: 'postgres',
-  dialectModule: pg, // Força o uso do módulo pg
+  dialectModule: pg,
   logging: false,
   dialectOptions: {
     ssl: {
